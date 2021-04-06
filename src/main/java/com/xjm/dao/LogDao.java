870dcebaf58a3838c553fa9f9b18128acb5b0012
JianMin-Xie <854377742@qq.com>
2021-04-07 07:16:14 +0800

    LogDao

--- src/main/java/com/xjm/dao/LogDao.java
+++ src/main/java/com/xjm/dao/LogDao.java
@@ -6,6 +6,29 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+/**
+ * Created by JianMin-xie on 2019/4/29.
+ */
+@Mapper
+public interface LogDao {
 
+    /**
+     * 添加日志
+     * @param logDomain
+     * @return
+     */
+    int addLog(LogDomain logDomain);
+
+    /**
+     * 删除日志
+     * @param id
+     * @return
+     */
+    int deleteLogById(@Param("id") Integer id);
+
+    /**
+     * 获取日志
+     * @return
+     */
     List<LogDomain> getLogs();
 }
